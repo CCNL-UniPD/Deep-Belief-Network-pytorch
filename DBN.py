a3652d2b81bb35a1af3a2fb469e1ae0fcb7bf53e
flavio2018 <flavio.petruzzellis@gmail.com>
2021-11-11 17:22:53 +0100

    Remove debug printout

--- DBN.py
+++ DBN.py
@@ -128,7 +128,6 @@ class DBN(nn.Module):
             v = tmp.view((tmp.shape[0], -1)).type(torch.FloatTensor)  # flatten
             if self.cuda():
                 v = v.cuda()
-            print(v.device)
             p_v, v = self.rbm_layers[i].forward(v)
             tmp = v
             # print(v.shape)
